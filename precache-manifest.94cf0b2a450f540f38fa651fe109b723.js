76ca41a6b2d08bf7787cdf3c327122943612bf8a
mdisabatino3 <mdisabatino3@gmail.com>
2019-11-11 12:48:45 -0500

    deploy

---
+++ precache-manifest.94cf0b2a450f540f38fa651fe109b723.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef8b1e0e92e6c02c2a9e98c9c0c86f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a34e71373a08d0609991",
+    "url": "/static/css/main.b3a3adf6.chunk.css"
+  },
+  {
+    "revision": "2069389e326c11da9aca",
+    "url": "/static/js/2.2dc9c37e.chunk.js"
+  },
+  {
+    "revision": "a34e71373a08d0609991",
+    "url": "/static/js/main.9a08e237.chunk.js"
+  },
+  {
+    "revision": "71c66f46860b867ffc99",
+    "url": "/static/js/runtime-main.d29d75e0.js"
+  }
+]);
