bfe4f36b609ab1c22b971143bdfd6aa01a2dee84
mdisabatino3 <mdisabatino3@gmail.com>
2020-07-17 22:30:20 -0400

    Faster loading of data

--- src/Corona.js
+++ src/Corona.js
@@ -18,8 +18,6 @@ export const Corona = (props) => {
 
     var g = svg.append("g");
 
-    var stateIds = [];
-
     var colorScale = d3.scaleLog()
       .interpolate(d3.interpolateRgb)
       .range([d3.rgb("#FFFFFF"), d3.rgb('#710019')]);
@@ -34,60 +32,118 @@ export const Corona = (props) => {
       .style("height", tooltipHeight)
       .style("width", tooltipWidth);
     // var usStates =d3.json("https://d3js.org/us-10m.v1.json").then(function(us) {
-    var usStates =d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/counties-albers-10m.json").then(function(us) {
 
+    const stateDataByFips = {};
+    d3.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv").then(function(stateData) {
+      stateData = stateData.sort(function(a,b) {
+        a = new Date(a.date);
+        b = new Date(b.date);
+        return a < b ? 1 : -1;
+      });
+
+      let usTotalCases = 0;
+      let usTotalDeaths = 0;
+      let usCasesYesterday = 0;
+      let usDeathsYesterday = 0;
+      let newCases = 0;
+      let newDeaths = 0;
+      stateData.forEach((el) => {
+        if(stateDataByFips[el.fips] == null) {
+            stateDataByFips[el.fips]=[el];
+          } else {
+            stateDataByFips[el.fips].push(el);
+          }
+      })
+      console.log(stateData);
+      console.log(stateDataByFips);
+      Object.values(stateDataByFips).forEach((el)=> {
+        usTotalCases = usTotalCases + parseInt(el[0].cases);
+        usTotalDeaths = usTotalDeaths + parseInt(el[0].deaths);
+        usCasesYesterday = usCasesYesterday + (el[1] != null ? parseInt(el[1].cases) : 0);
+        usDeathsYesterday = usDeathsYesterday + (el[1] != null ? parseInt(el[1].deaths) : 0);
+      })
+      newCases = usTotalCases - usCasesYesterday;
+      newDeaths = usTotalDeaths - usDeathsYesterday;
+
+      let paTotalCases = stateDataByFips["42"][0].cases;
+      let paCasesYesterday = stateDataByFips["42"][1].cases;
+      let paTotalDeaths = stateDataByFips["42"][0].deaths;
+      let paDeathsYesterday = stateDataByFips["42"][1].deaths;
+      let paNewCases = parseInt(paTotalCases) - parseInt(paCasesYesterday);
+      let paNewDeaths = parseInt(paTotalDeaths) - parseInt(paDeathsYesterday);
+    }); 
+    const countyDataByFips = {}
+    const stateIds = []
+    var usStates = d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/counties-albers-10m.json").then(function(us) {
       us.objects.states.geometries.forEach(geometry => {
-        stateIds.push({
-          id: geometry.id,
-          name: geometry.properties.name 
-        })
+        stateIds[geometry.id] = geometry.properties.name;
       })
 
-      stateIds.sort((a,b) => (a.id > b.id) ? 1 : -1);
-
       d3.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv").then(function(covidData) {
-        console.log("covid data " + covidData);
+        covidData = covidData.sort(function(a,b) {
+          a = new Date(a.date);
+          b = new Date(b.date);
+          return a < b ? 1 : -1;
+        });
+        covidData.forEach((el) => {
+          if(countyDataByFips[el.fips] == null) {
+            countyDataByFips[el.fips]=[el];
+          } else {
+            countyDataByFips[el.fips].push(el);
+          }
+          if(el.county == "New York City" || el.county == "Kansas City") {
+            if (countyDataByFips[el.county] == null) {
+              countyDataByFips[el.county] = [el];
+            } else {
+              countyDataByFips[el.county].push(el);
+            }
+          }
+        });
+        
         var extent = d3.extent(covidData, function(d) {return parseInt(d.cases);})
+        // extent[1] = extent[1] * .15
         extent[0] = 1;
-        colorScale.domain(extent);
+        var colorScale = d3.scaleLog()
+          .domain(extent)
+          .range(["#FFFFFF", '#710019'])
+        console.log("extent " + extent);
         us.objects.counties.geometries.forEach(function(d) {
+          
           d.properties.id = d.id;
           d.properties.stateId = d.id.toString().slice(0,2);
-          let state = stateIds.find(el => el.id === d.properties.stateId)
-          d.properties.stateName = state.name;
+          d.properties.stateName = stateIds[d.properties.stateId];
           let exceptional = getCaseDataForExceptions(d.properties.id);
           let caseData;
+          let newCases;
+          let newDeaths;
           let color;
-          if (exceptional !== null) {
-            caseData = covidData.filter(el => el.county === exceptional[0]).sort(function(a,b) {
-              a = new Date(a.date);
-              b = new Date(b.date);
-              return a < b ? 1 : -1;
-            });
-            if (caseData.length !== 0) {
-              caseData = caseData[0];
-            } else {
-              caseData = null;
-            }
-            color = colorScale(caseData !== null ? caseData.cases : 0);
+          if (exceptional != null) {
+            caseData = countyDataByFips[exceptional[0]] != null ? countyDataByFips[exceptional[0]] : null;
           } else {
-            caseData = covidData.filter(el => el.fips === d.properties.id).sort(function(a,b) {
-              a = new Date(a.date);
-              b = new Date(b.date);
-              return a < b ? 1 : -1;
-            });
-            if (caseData.length !== 0) {
-              caseData = caseData[0];
-            } else {
-              caseData = null;
+            if (d.properties.id == "36029") {
+              console.log("Erie data" + console.log(countyDataByFips[d.properties.id]))
             }
-            color = colorScale(caseData !== null ? caseData.cases : 0);
+            caseData = countyDataByFips[d.properties.id] != null ? countyDataByFips[d.properties.id] : null;
           }
+          if (caseData != null) {
+            let casesToday = caseData[0].cases;
+            let deathsToday = caseData[0].deaths;
+            let casesYesterday = caseData[1] != null ? caseData[1].cases : 0;
+            let deathsYesterday = caseData[1] != null ? caseData[1].deaths : 0;
+
+            newCases = parseInt(casesToday) - parseInt(casesYesterday)
+            newDeaths = parseInt(deathsToday) - parseInt(deathsYesterday)
+          } else {
+            newCases = 0;
+            newDeaths = 0;
+          }
+          color = colorScale(caseData != null ? parseInt(caseData[0].cases) : 0);
           d.properties.caseData = caseData;
           d.properties.color = color;
+          d.properties.newCases = newCases;
+          d.properties.newDeaths = newDeaths;
         })
         setColors();
-
       });
 
       g.selectAll("path")
@@ -122,15 +178,19 @@ export const Corona = (props) => {
         let countyId = dataProperties.id;
         let countyName = dataProperties.name;
         let stateName = dataProperties.stateName;
-        let numCases = dataProperties.caseData !== null ? dataProperties.caseData.cases : "no cases reported";
-        let numDeaths = dataProperties.caseData !== null ? dataProperties.caseData.deaths : "no deaths reported";
+        let numCases = dataProperties.caseData !== null ? dataProperties.caseData[0].cases : "no cases reported";
+        let numDeaths = dataProperties.caseData !== null ? dataProperties.caseData[0].deaths : "no deaths reported";
+        let newCases = dataProperties.newCases;
+        let newDeaths = dataProperties.newDeaths;
         let exceptional = getCaseDataForExceptions(countyId);
         if (exceptional === null) {
           tooltip
               .style("opacity", 0.9);    
               tooltip.html("<p><strong>" + dataProperties.name + ", " + dataProperties.stateName + "</strong><p>" +
               "<p>Cases: " + numCases + "</p>" +
-              "<p>Deaths: " + numDeaths)  
+              "<p>Deaths: " + numDeaths +
+              "<p>New cases: " + newCases + "</p>" +
+              "<p>New deaths: " + newDeaths + "</p>")
               // .style("left", (d3.event.pageX - offsetLeft - tooltipWidth*.8) + "px")   
               // .style("top", (d3.event.pageY - offsetTop - tooltipHeight*1.5) + "px");
               .style("left",d3.event.pageX + "px")
@@ -140,7 +200,9 @@ export const Corona = (props) => {
               .style("opacity", 0.9);    
               tooltip.html("<p><strong>" + dataProperties.name + ", " + dataProperties.stateName + "</strong><p>" +
               "<p>Cases for " + exceptional[0] + ": " + numCases + "</p>" +
-              "<p>Deaths for " + exceptional[0] + ": " + numDeaths)  
+              "<p>Deaths for " + exceptional[0] + ": " + numDeaths + "</p>" +
+              "<p>New cases: " + newCases + "</p>" +
+              "<p>New deaths: " + newDeaths + "</p>")
               // .style("left", (d3.event.pageX - offsetLeft - tooltipWidth*0.8) + "px")   
               // .style("top", (d3.event.pageY - offsetTop - tooltipHeight*1.5) + "px");
               .style("left",d3.event.pageX + "px")
